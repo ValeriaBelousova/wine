259d18cb3a196f25c9466f5163bbe92b70919579
lerbel <Kaleria.Belousova@yandex.ru>
2023-04-05 01:53:41 +0300

    Updates

---
+++ precache-manifest.3facc60477b81e4828e1c553e68251b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "488e3f887d4dedf9687c5cd3fc43af9a",
+    "url": "/wine/index.html"
+  },
+  {
+    "revision": "061641651bd2d8832d46",
+    "url": "/wine/static/css/2.2b597d09.chunk.css"
+  },
+  {
+    "revision": "d93253df9f1ec5b64df2",
+    "url": "/wine/static/css/main.1096087d.chunk.css"
+  },
+  {
+    "revision": "061641651bd2d8832d46",
+    "url": "/wine/static/js/2.225f1364.chunk.js"
+  },
+  {
+    "revision": "d93253df9f1ec5b64df2",
+    "url": "/wine/static/js/main.a7fec61e.chunk.js"
+  },
+  {
+    "revision": "44742330e2fd0d9db3a3",
+    "url": "/wine/static/js/runtime-main.13038b02.js"
+  },
+  {
+    "revision": "ae41f2f044aeed69aebc48860c50c178",
+    "url": "/wine/static/media/NoahText-Bold.ae41f2f0.ttf"
+  },
+  {
+    "revision": "73000660b3eef18604299323e9049cc0",
+    "url": "/wine/static/media/NoahText-Regular.73000660.ttf"
+  }
+]);
