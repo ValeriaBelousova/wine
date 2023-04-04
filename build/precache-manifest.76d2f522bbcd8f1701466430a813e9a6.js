8a58b235687b32d8ed9d04e7e4e6685101bfc39c
lerbel <Kaleria.Belousova@yandex.ru>
2023-04-05 02:19:51 +0300

    deploy

---
+++ build/precache-manifest.76d2f522bbcd8f1701466430a813e9a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b29564bbad9ad6eb1754cd12eef8616f",
+    "url": "/wine/index.html"
+  },
+  {
+    "revision": "061641651bd2d8832d46",
+    "url": "/wine/static/css/2.2b597d09.chunk.css"
+  },
+  {
+    "revision": "382470f294a536a6c285",
+    "url": "/wine/static/css/main.1096087d.chunk.css"
+  },
+  {
+    "revision": "061641651bd2d8832d46",
+    "url": "/wine/static/js/2.225f1364.chunk.js"
+  },
+  {
+    "revision": "382470f294a536a6c285",
+    "url": "/wine/static/js/main.951340d8.chunk.js"
+  },
+  {
+    "revision": "44742330e2fd0d9db3a3",
+    "url": "/wine/static/js/runtime-main.13038b02.js"
+  },
+  {
+    "revision": "ae41f2f044aeed69aebc48860c50c178",
+    "url": "/wine/static/media/NoahText-Bold.ae41f2f0.ttf"
+  },
+  {
+    "revision": "73000660b3eef18604299323e9049cc0",
+    "url": "/wine/static/media/NoahText-Regular.73000660.ttf"
+  }
+]);
