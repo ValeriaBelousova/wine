f380e6d6f8fa375f9fa1fab45618c31d7397cd21
ValeriaBelousova <kaleria.belousova@yandex.ru>
2023-04-18 22:39:30 +0300

    deploy

---
+++ build/precache-manifest.39e34504e02f50a63e5d9aa2dd85a33e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "122f68fed889de7e15b79e79042b6ebe",
+    "url": "/wine/index.html"
+  },
+  {
+    "revision": "dcd957aca2a998252f64",
+    "url": "/wine/static/css/2.2b597d09.chunk.css"
+  },
+  {
+    "revision": "9bf2be5d2a2faa39043b",
+    "url": "/wine/static/css/main.c00c3f33.chunk.css"
+  },
+  {
+    "revision": "dcd957aca2a998252f64",
+    "url": "/wine/static/js/2.a57e14e9.chunk.js"
+  },
+  {
+    "revision": "9bf2be5d2a2faa39043b",
+    "url": "/wine/static/js/main.b1829884.chunk.js"
+  },
+  {
+    "revision": "44742330e2fd0d9db3a3",
+    "url": "/wine/static/js/runtime-main.13038b02.js"
+  },
+  {
+    "revision": "73000660b3eef18604299323e9049cc0",
+    "url": "/wine/static/media/NoahText-Regular.73000660.ttf"
+  }
+]);
